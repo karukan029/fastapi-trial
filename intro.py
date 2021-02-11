b60aa806bbc10c6ef953828131899d5ed08d6a46
karukan013 <karukan029@gmail.com>
2021-02-11 18:03:22 +0900

    intro end

--- intro.py
+++ intro.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/') # methodとendpointの指定
-async def hello():
-    return {'text': 'hello world!'}
+# @app.get('/') # methodとendpointの指定
+# async def hello():
+#     return {"text": "hello world!"}
+
+@app.get('/get/{path}')
+async def path_and_query_params(
+        path: str,
+        query: int,
+        default_none: Optional[str] = None):
+    return {"text":f"hello,{path}, {query} and {default_none}"}
+
+class ItemOut(BaseModel):
+    strings: str
+    aux: int = 1
+    text: str
+
+@app.get('/', response_model=ItemOut)
+async def response(strings: str, integer: int):
+    return {"text": "hello world!", "strings": strings, "integer": integer}
